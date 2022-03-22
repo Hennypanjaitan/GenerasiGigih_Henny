9eed1a5e71e72763f8ffd11e2edf65597eec3dd6
Henny Panjaitan <hennypanjaitan5@gmail.com>
2022-03-22 22:07:22 +0700

    Submit homework update app.js and folder pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import dataJson from './pages/home/index';
+import data from './pages/home/index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Track Info</h2>
+        <img src={dataJson.album.images[0].url} width="250" height="200" className="rotate" />
+      </div>
+      <div>
+        <h2>{dataJson.name}</h2>
+        <h3>{dataJson.album.name}</h3>
+        <p>{dataJson.artists[0].name}</p>
+      	<button><a href={dataJson.album.artists[0].external_urls.spotify}>SELECT</a></button>
+      </div>
     </div>
   );
 }
