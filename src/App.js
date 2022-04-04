2d896383c48ef6e13b97d5047f0e2f09599ebdeb
Henny Panjaitan <hennypanjaitan5@gmail.com>
2022-04-04 07:56:41 +0700

    Homework Module 3 Session 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-import './App.css';
-import { Playlist } from './pages/homework';
-
-function App() {
-  return (
-    <div>
-      <Playlist />
-    </div>
-  );
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-export default App;
